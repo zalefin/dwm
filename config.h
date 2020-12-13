3c0a3106090291f8eea90b483bb5bce15135a391
Zach Lefin <zachlefin@gmail.com>
2020-12-12 19:27:35 -0700

    change to gruvbox colors

--- config.h
+++ config.h
@@ -13,15 +13,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Sauce Code Pro Nerd Font:size=10" };
 static const char dmenufont[]       = "Sauce Code Pro Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char gruvbox_bg0_h[] = "#1d2021";
+static const char gruvbox_bg[] = "#282828";
+static const char gruvbox_orange[] = "#fe8019";
+static const char gruvbox_fg[] = "#ebdbb2";
+
 static const char *colors[][3]      = {
+
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { gruvbox_fg, gruvbox_bg0_h, gruvbox_bg },
+	[SchemeSel]  = { gruvbox_bg, gruvbox_fg, gruvbox_orange },
 };
 
 /* tagging */
@@ -64,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", gruvbox_bg0_h, "-nf", gruvbox_fg, "-sb", gruvbox_fg, "-sf", gruvbox_bg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
